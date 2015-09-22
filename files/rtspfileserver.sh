b323cf6f00c74df18d481696903654866c0bfe82
RuanJG <pi@raspberrypi.(none)>
2015-09-22 13:27:39 +0000

    Ruan: little modify for rasberry

--- files/rtspfileserver.sh
+++ files/rtspfileserver.sh
@@ -1,6 +1,15 @@
 #!/bin/sh 
+net='wlan0'
 while [ 0 ]
 do
-	/bin/rtspfileserver 320 240
+	ip=$(ifconfig $net | grep 'inet addr')
+	ip=${ip#*'inet addr:'}
+	ip=${ip%' Bcast:'*}
+	if [ "$ip"'x' = 'x' ];then
+		echo can not find ip in br-lan
+	else
+        	echo "start rtspfileserver, in $ip"
+		/bin/rtspfileserver 320 240
+	fi
 	sleep 3
 done
